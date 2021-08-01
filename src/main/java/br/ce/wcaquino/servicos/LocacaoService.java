ace13996ef24f664da7148aceec5e9d6bd694f52
gabdevaraujo <biel.vianadearaujo@gmail.com>
2021-08-01 12:50:48 -0300

    Aula-03---Testando Sem Ferramenta

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -2,54 +2,48 @@ package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
+import java.util.Date;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
-import java.util.Date;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
 
-  public Locacao alugarFilme(Usuario usuario, Filme filme) {
-    Locacao locacao = new Locacao();
-    locacao.setFilme(filme);
-    locacao.setUsuario(usuario);
-    locacao.setDataLocacao(new Date());
-    locacao.setValor(filme.getPrecoLocacao());
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
 
-    //Entrega no dia seguinte
-    Date dataEntrega = new Date();
-    dataEntrega = adicionarDias(dataEntrega, 1);
-    locacao.setDataRetorno(dataEntrega);
+	public static void main(String[] args) {
+		
+		//cenario
+		LocacaoService locacaoService = new LocacaoService();
+		
+		Usuario usuario = new Usuario("Teste");
+		Filme filme = new Filme("Teste FIlme", 5, 10.00);
+		
+		//acao
+		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
-    //Salvando a locacao...
-    //TODO adicionar método para salvar
 
-    return locacao;
-  }
-
-  @Test
-  public void teste() {
-    //cenario
-    LocacaoService service = new LocacaoService();
-    Usuario usuario = new Usuario("Usuario 1");
-    Filme filme = new Filme("Filme 1", 2, 5.0);
-
-    //acao
-    Locacao locacao = service.alugarFilme(usuario, filme);
-
-    //verificacao
-    Assert.assertTrue(locacao.getValor() == 5.0);
-    Assert.assertTrue(
-      DataUtils.isMesmaData(locacao.getDataLocacao(), new Date())
-    );
-    Assert.assertTrue(
-      DataUtils.isMesmaData(
-        locacao.getDataRetorno(),
-        DataUtils.obterDataComDiferencaDias(1)
-      )
-    );
-  }
+		//verificaoção
+		System.out.println(locacao.getValor() == 10.00);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(2)));
+	}
 }
